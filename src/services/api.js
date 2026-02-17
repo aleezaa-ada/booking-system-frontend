1c92b863a080e5ede23138c71622967dad085ec3
Aleeza Ahmed <aleezaahmed@deloitte.co.uk>
2026-02-17 19:03:57 +0000

    feat: Add Axios API service with authentication interceptor

---
+++ src/services/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL; 
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// request interceptor to include the token in headers
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Token ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
